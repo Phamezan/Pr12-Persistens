388e6302ea2e1582c70568c1049d4ae647e5d0f1
Phamezan <pham2003@outlook.dk>
2024-10-30 21:47:42 +0100

    Ændrede i LoadPerson() Test fordi programmet ændre punktum til komma hvilket gjorde at min test fejlede

---
+++ Persistens/DataHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Globalization;
+namespace Persistens
+{
+    public class DataHandler
+    {
+        private string _dataFileName;
+        public string DataFileName { get; }
+
+        public DataHandler(string dataFileName)
+        {
+            DataFileName = dataFileName;
+        }
+
+        public void SavePerson(Person person)
+        {
+            StreamWriter wr = new StreamWriter("Data.txt");
+            wr.WriteLine($"{person.Name}, {person.BirthDate},{person.Height},{person.IsMarried},{person.NoOfChildren}");
+            wr.Close();
+        }
+
+        public Person LoadPerson()
+        {
+            StreamReader sr = new StreamReader("Data.txt");
+            string line = sr.ReadLine();
+            if (line != null)
+            {
+                string[] split = line.Split(",");
+                Person person = new Person(split[0], DateTime.Parse(split[1]), double.Parse(split[2]), bool.Parse(split[3]), int.Parse(split[4]));
+                sr.Close();
+                return person;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Persistens/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace Persistens
+{
+    public class Person
+    {
+        private string _name;
+        private DateTime _birthDate;
+        private double _height;
+        private bool _isMarried;
+        private int _noOfChildren;
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public double Height { get; set; }
+
+        public bool IsMarried { get; set; }
+
+        public int NoOfChildren { get; set; }
+
+        public Person(string name, DateTime _birthDate, double height, bool _isMarried, int _noOfChildren)
+        {
+            Name = name;
+            BirthDate = _birthDate;
+            Height = height;
+            IsMarried = _isMarried;
+            NoOfChildren = _noOfChildren;
+        }
+
+        public string MakeTitle()
+        {
+            return $"{Name};{BirthDate};{Height};{IsMarried};{NoOfChildren}";
+        }
+    }
+}
